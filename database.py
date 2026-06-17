694b7a5819016f52fb868376e60e24a013cb20ec
Merwin <merwinrojer904@gmail.com>
2026-06-17 23:33:05 +0530

    Use environment variable for database URL

--- database.py
+++ database.py
@@ -1,7 +1,12 @@
+import os
+
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker, declarative_base
 
-DATABASE_URL = "postgresql://postgres:postgres123@host.docker.internal:5432/resume_analyzer_db"
+DATABASE_URL = os.getenv(
+    "DATABASE_URL",
+    "postgresql://postgres:postgres123@host.docker.internal:5432/resume_analyzer_db"
+)
 
 engine = create_engine(DATABASE_URL)
 
