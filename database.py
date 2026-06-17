b37de6192e3e0dffe5837cd4e4cb9fb9774d4c7b
Merwin <merwinrojer904@gmail.com>
2026-06-17 22:46:40 +0530

    Add Docker support

--- database.py
+++ database.py
@@ -1,7 +1,7 @@
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker, declarative_base
 
-DATABASE_URL = "postgresql://postgres:postgres123@localhost:5432/resume_analyzer_db"
+DATABASE_URL = "postgresql://postgres:postgres123@host.docker.internal:5432/resume_analyzer_db"
 
 engine = create_engine(DATABASE_URL)
 
